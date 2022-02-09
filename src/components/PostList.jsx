5daf48e86baa2902b190b3ef178186fbf5897d44
Ira <mamlin.ira@gmail.com>
2022-02-09 16:34:40 +0200

    added transition posts

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { TransitionGroup } from "react-transition-group";
 import PostItem from "./PostItem";
+import {CSSTransition} from 'react-transition-group';
 
 const PostList = (props) => {
     if (!props.posts.length) {
@@ -12,9 +14,14 @@ const PostList = (props) => {
         <h1 style={{textAlign: "center"}}>
             {props.title}
         </h1>
-        {props.posts.map((post, index) => 
-            <PostItem remove={props.remove} number={index + 1} post={post} key={post.id} />)
-        }
+        <TransitionGroup>
+            {props.posts.map((post, index) => 
+            <CSSTransition key={post.id} timeout={500} classNames="post">
+                <PostItem remove={props.remove} number={index + 1} post={post}  />
+            </CSSTransition>    
+            )
+            }
+        </TransitionGroup>  
       </div>
     )
     
